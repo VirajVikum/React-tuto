74e00c961d9d03556517e977f2800ec3b289083e
Viraj Vikum <virajempire2001@gmail.com>
2025-01-29 21:04:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PartOne from './components/PartOne';
+
+function App() {
+  return (
+    <div className="App">
+      <PartOne />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PartOne.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PartOne() {
+  return (
+    <div>PartOne</div>
+  )
+}
